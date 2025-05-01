a7c4bb43e674e066e2a1dc3d638776e09b056b88
Phuong Nguyen <phuong.fi.91@gmail.com>
2025-05-01 11:27:27 +0300

    chore: Clean up number handlers

--- django2pydantic/handlers/auto.py
+++ django2pydantic/handlers/auto.py
@@ -1,13 +1,14 @@
 """Auto field handlers."""
+# pylint: disable=too-few-public-methods
 
 from typing import override
 
 from django.db import models
 
-from django2pydantic.handlers.base import DjangoFieldHandler
+from django2pydantic.handlers.numbers import DjangoIntegerFieldHandler
 
 
-class SmallAutoFieldHandler(DjangoFieldHandler[models.SmallAutoField[int, int]]):
+class SmallAutoFieldHandler(DjangoIntegerFieldHandler[models.SmallAutoField[int, int]]):
     """Handler for Small Auto fields."""
 
     @classmethod
@@ -15,24 +16,8 @@ class SmallAutoFieldHandler(DjangoFieldHandler[models.SmallAutoField[int, int]])
     def field(cls) -> type[models.SmallAutoField[int, int]]:
         return models.SmallAutoField
 
-    @property
-    @override
-    def ge(self) -> int | None:
-        validator_min = super().ge
-        return max(1, validator_min) if validator_min is not None else 1
 
-    @property
-    @override
-    def le(self) -> int | None:
-        validator_max = super().le
-        return min(32767, validator_max) if validator_max is not None else 32767
-
-    @override
-    def get_pydantic_type_raw(self) -> type[int]:
-        return int
-
-
-class AutoFieldHandler(DjangoFieldHandler[models.AutoField[int, int]]):
+class AutoFieldHandler(DjangoIntegerFieldHandler[models.AutoField[int, int]]):
     """Handler for Auto fields."""
 
     @classmethod
@@ -40,60 +25,11 @@ class AutoFieldHandler(DjangoFieldHandler[models.AutoField[int, int]]):
     def field(cls) -> type[models.AutoField[int, int]]:
         return models.AutoField
 
-    @property
-    @override
-    def ge(self) -> int | None:
-        validator_min = super().ge
-        return max(1, validator_min) if validator_min is not None else 1
 
-    @property
-    @override
-    def le(self) -> int | None:
-        validator_max = super().le
-        return (
-            min(2147483647, validator_max) if validator_max is not None else 2147483647
-        )
-
-    @override
-    def get_pydantic_type_raw(self) -> type[int]:
-        return int
-
-
-class BigAutoFieldHandler(DjangoFieldHandler[models.BigAutoField[int, int]]):
+class BigAutoFieldHandler(DjangoIntegerFieldHandler[models.BigAutoField[int, int]]):
     """Handler for Big Auto fields."""
 
     @classmethod
     @override
     def field(cls) -> type[models.BigAutoField[int, int]]:
         return models.BigAutoField
-
-    @property
-    @override
-    def ge(self) -> int | None:
-        validator_min = super().ge
-        return max(1, validator_min) if validator_min is not None else 1
-
-    @property
-    @override
-    def le(self) -> int | None:
-        validator_max = super().le
-        return (
-            min(9223372036854775807, validator_max)
-            if validator_max is not None
-            else 9223372036854775807
-        )
-
-    @property
-    @override
-    def examples(self) -> list[int]:
-        if self.ge is not None and self.le is not None:
-            return [self.ge, self.le]
-        if self.ge is not None:
-            return [self.ge]
-        if self.le is not None:
-            return [self.le]
-        return []
-
-    @override
-    def get_pydantic_type_raw(self) -> type[int]:
-        return int
--- django2pydantic/handlers/numbers.py
+++ django2pydantic/handlers/numbers.py
@@ -1,154 +1,119 @@
+"""Numbers field handlers."""
+# pylint: disable=too-few-public-methods
+
 from decimal import Decimal
-from typing import override
+from typing import Generic, cast, override
 
 from django.db import connection, models
 
-from django2pydantic.handlers.base import DjangoFieldHandler
+from django2pydantic.handlers.base import DjangoFieldHandler, TDjangoField_co
 
 
-class IntegerFieldHandler(DjangoFieldHandler[models.IntegerField[int, int]]):
-    """Handler for Integer fields."""
+class DjangoIntegerFieldHandler(
+    Generic[TDjangoField_co], DjangoFieldHandler[TDjangoField_co]
+):
+    @property
+    @override
+    def ge(self) -> int | None:
+        validator_min = super().ge
+        db_min = connection.ops.integer_field_range(self.field().__name__)[0]
+        return max(db_min, validator_min) if validator_min is not None else db_min
+
+    @property
+    @override
+    def le(self) -> int | None:
+        validator_max = super().le
+        db_max = connection.ops.integer_field_range(self.field().__name__)[1]
+        return min(db_max, validator_max) if validator_max is not None else db_max
 
     @override
+    def get_pydantic_type_raw(self) -> type[int]:
+        return int
+
+    # TODO(phuongfi91): This might cause issue with schemathesis test
+    #  https://github.com/NextGenContributions/django-ninja-crudl/issues/35
+    # examples = [-9223372036854775808, 9223372036854775807]
+    # unresolved_definition = [-9223372036854775808, 9223372036854775807]
+    #
+    #     def extract_inner_examples(
+    #         examples: dict[str, Any], unresolved_definition: dict[str, Any]
+    #     ) -> Generator[Any, None, None]:
+    #         """Extract exact examples values from the `examples` dictionary."""
+    # >       for name, example in examples.items():
+    # E       AttributeError: 'list' object has no attribute 'items'
+    #
+    # ../.venv/lib/python3.12/site-packages/schemathesis/specs/openapi/examples.py:209: AttributeError
+    #
+    # @property
+    # @override
+    # def examples(self) -> list[int]:
+    #     if self.ge is not None and self.le is not None:
+    #         return [self.ge, self.le]
+    #     if self.ge is not None:
+    #         return [self.ge]
+    #     if self.le is not None:
+    #         return [self.le]
+    #     return []
+
+
+class IntegerFieldHandler(DjangoIntegerFieldHandler[models.IntegerField[int, int]]):
+    """Handler for Integer fields."""
+
     @classmethod
+    @override
     def field(cls) -> type[models.IntegerField[int, int]]:
         return models.IntegerField
 
-    # TODO(phuongfi91): Revisit all the potential None values in mix and max params
-    @property
-    @override
-    def ge(self) -> int | None:
-        return max(connection.ops.integer_field_range("IntegerField")[0], super().ge)
 
-    @property
-    @override
-    def le(self) -> int | None:
-        return min(connection.ops.integer_field_range("IntegerField")[1], super().le)
-
-    @override
-    def get_pydantic_type_raw(self) -> type[int]:
-        return int
-
-
-class SmallIntegerFieldHandler(DjangoFieldHandler[models.SmallIntegerField[int, int]]):
+class SmallIntegerFieldHandler(
+    DjangoIntegerFieldHandler[models.SmallIntegerField[int, int]]
+):
     """Handler for Small Integer fields."""
 
-    @override
     @classmethod
+    @override
     def field(cls) -> type[models.SmallIntegerField[int, int]]:
         return models.SmallIntegerField
 
-    @property
-    @override
-    def ge(self) -> int | None:
-        return max(
-            connection.ops.integer_field_range("SmallIntegerField")[0],
-            super().ge,
-        )
-
-    @property
-    @override
-    def le(self) -> int | None:
-        return min(
-            connection.ops.integer_field_range("SmallIntegerField")[1],
-            super().le,
-        )
-
-    @override
-    def get_pydantic_type_raw(self) -> type[int]:
-        return int
-
 
 class PositiveSmallIntegerFieldHandler(
-    DjangoFieldHandler[models.PositiveSmallIntegerField[int, int]],
+    DjangoIntegerFieldHandler[models.PositiveSmallIntegerField[int, int]],
 ):
     """Handler for Positive Small Integer fields.
 
     Ref: https://docs.djangoproject.com/en/5.1/ref/models/fields/#positivesmallintegerfield
     """
 
-    @override
     @classmethod
+    @override
     def field(cls) -> type[models.PositiveSmallIntegerField[int, int]]:
         return models.PositiveSmallIntegerField
 
-    @property
-    @override
-    def ge(self) -> int | None:
-        return max(
-            connection.ops.integer_field_range("PositiveSmallIntegerField")[0],
-            super().ge,
-        )
-
-    @property
-    @override
-    def le(self) -> int | None:
-        return min(
-            connection.ops.integer_field_range("PositiveSmallIntegerField")[1],
-            super().le,
-        )
-
-    @override
-    def get_pydantic_type_raw(self) -> type[int]:
-        return int
-
 
 class PositiveIntegerFieldHandler(
-    DjangoFieldHandler[models.PositiveIntegerField[int, int]]
+    DjangoIntegerFieldHandler[models.PositiveIntegerField[int, int]]
 ):
     """Handler for Positive Integer fields."""
 
-    @override
     @classmethod
+    @override
     def field(cls) -> type[models.PositiveIntegerField[int, int]]:
         return models.PositiveIntegerField
 
-    @property
-    @override
-    def ge(self) -> int | None:
-        return max(
-            connection.ops.integer_field_range("PositiveIntegerField")[0],
-            super().ge,
-        )
 
-    @property
-    @override
-    def le(self) -> int | None:
-        return min(
-            connection.ops.integer_field_range("PositiveIntegerField")[1],
-            super().le,
-        )
-
-    @override
-    def get_pydantic_type_raw(self) -> type[int]:
-        return int
-
-
-class BigIntegerFieldHandler(DjangoFieldHandler[models.BigIntegerField[int, int]]):
+class BigIntegerFieldHandler(
+    DjangoIntegerFieldHandler[models.BigIntegerField[int, int]]
+):
     """Handler for Big Integer fields."""
 
-    @override
     @classmethod
+    @override
     def field(cls) -> type[models.BigIntegerField[int, int]]:
         return models.BigIntegerField
 
-    @property
-    @override
-    def ge(self) -> int | None:
-        return max(connection.ops.integer_field_range("BigIntegerField")[0], super().ge)
-
-    @property
-    @override
-    def le(self) -> int | None:
-        return min(connection.ops.integer_field_range("BigIntegerField")[1], super().le)
-
-    @override
-    def get_pydantic_type_raw(self) -> type[int]:
-        return int
-
 
 class PositiveBigIntegerFieldHandler(
-    DjangoFieldHandler[models.PositiveBigIntegerField[int, int]],
+    DjangoIntegerFieldHandler[models.PositiveBigIntegerField[int, int]],
 ):
     """Handler for Positive Big Integer fields."""
 
@@ -156,26 +121,6 @@ class PositiveBigIntegerFieldHandler(
     @override
     def field(cls) -> type[models.PositiveBigIntegerField[int, int]]:
         return models.PositiveBigIntegerField
-
-    @property
-    @override
-    def ge(self) -> int | None:
-        return max(
-            connection.ops.integer_field_range("PositiveBigIntegerField")[0],
-            super().ge or 0,
-        )
-
-    @property
-    @override
-    def le(self) -> int | None:
-        return min(
-            connection.ops.integer_field_range("PositiveBigIntegerField")[1],
-            super().le or 9223372036854775808,
-        )
-
-    @override
-    def get_pydantic_type_raw(self) -> type[int]:
-        return int
 
 
 class DecimalFieldHandler(DjangoFieldHandler[models.DecimalField[Decimal, Decimal]]):
@@ -189,12 +134,14 @@ class DecimalFieldHandler(DjangoFieldHandler[models.DecimalField[Decimal, Decima
     @property
     @override
     def max_digits(self) -> int | None:
-        return self.field_obj.max_digits
+        return cast("models.DecimalField[Decimal, Decimal]", self.field_obj).max_digits
 
     @property
     @override
     def decimal_places(self) -> int | None:
-        return self.field_obj.decimal_places
+        return cast(
+            "models.DecimalField[Decimal, Decimal]", self.field_obj
+        ).decimal_places
 
     @override
     def get_pydantic_type_raw(self) -> type[Decimal]:
