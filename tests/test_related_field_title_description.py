d994286cb643880678272ee472d215204c05d01b
Jukka Hassinen <jukka.hassinen@gmail.com>
2025-06-03 08:07:33 +0300

    Fix reverse relation title and description (#85)
    * fix: reverse relation title and description
    
    * fix: reverse relational field titles uses the related models' verbose_name and verbose_name_plural
    
    + enabled a few skipped tests
    + improved one error message related to fields that are not found
    
    ---------
    
    Co-authored-by: Phuong Nguyen <7949163+phuongfi91@users.noreply.github.com>

--- tests/test_related_field_title_description.py
+++ tests/test_related_field_title_description.py
@@ -1,6 +1,6 @@
 """Test cases for Django models to Pydantic models conversion."""
 
-from typing import Any
+from typing import Any, final
 
 from django.db import models
 
@@ -21,6 +21,7 @@ def test_related_field_description_is_set_from_help_text() -> None:
     """Test that the field description is set from the help text if it exists."""
     help_text = "This is a test help text for the field."
 
+    @final
     class TestModel(models.Model):
         field = models.ForeignKey(
             help_text=help_text,
@@ -29,11 +30,10 @@ def test_related_field_description_is_set_from_help_text() -> None:
             related_name="related_field",
         )
 
-        class Meta:
-            app_label = "test_app"
-
-    class FieldSchema(BaseSchema):
-        config = SchemaConfig(model=TestModel, fields={"id": Infer, "field": Infer})
+    class FieldSchema(BaseSchema[TestModel]):
+        config: SchemaConfig[TestModel] = SchemaConfig(
+            model=TestModel, fields={"id": Infer, "field": Infer}
+        )
 
     openapi_schema = FieldSchema.model_json_schema()
     assert (
@@ -46,6 +46,7 @@ def test_related_field_title_is_set_from_verbose_name() -> None:
     """Test that the field title is set from the verbose name if it exists."""
     title = "This is a test title for the field."
 
+    @final
     class TestModel(models.Model):
         field = models.ForeignKey(
             verbose_name=title,
@@ -54,11 +55,99 @@ def test_related_field_title_is_set_from_verbose_name() -> None:
             related_name="related_field",
         )
 
-        class Meta:
-            app_label = "test_app"
-
-    class FieldSchema(BaseSchema):
-        config = SchemaConfig(model=TestModel, fields={"id": Infer, "field": Infer})
+    class FieldSchema(BaseSchema[TestModel]):
+        config: SchemaConfig[TestModel] = SchemaConfig[TestModel](
+            model=TestModel, fields={"id": Infer, "field": Infer}
+        )
 
     openapi_schema = FieldSchema.model_json_schema()
     assert openapi_schema["properties"]["field"]["title"].strip() == title.strip()
+
+
+def test_OneToOneField_reverse_relationship_title_is_set_from_verbose_name() -> None:
+    """Test that the reverse relation field title is set from the verbose name."""
+    title = "This is a test title for the reverse relationship field."
+    related_name = "b_set"
+
+    @final
+    class A(models.Model):
+        pass
+
+    @final
+    class B(models.Model):  # pyright: ignore[reportUnusedClass]
+        a = models.OneToOneField(
+            A,
+            on_delete=models.CASCADE,
+            related_name=related_name,
+        )
+
+        @final
+        class Meta:
+            verbose_name = title
+
+    class FieldSchema(BaseSchema[A]):
+        config: SchemaConfig[A] = SchemaConfig(
+            model=A, fields={"id": Infer, related_name: Infer}
+        )
+
+    openapi_schema = FieldSchema.model_json_schema()
+    assert openapi_schema["properties"][related_name]["title"].strip() == title.strip()
+
+
+def test_ManyToManyField_reverse_relationship_title_is_set_from_verbose_name() -> None:
+    """Test that the reverse relation field title is set from the verbose name."""
+    title = "This is a test title for the reverse relationship field."
+    related_name = "b_set"
+
+    @final
+    class A(models.Model):
+        pass
+
+    @final
+    class B(models.Model):  # pyright: ignore[reportUnusedClass]
+        a = models.ManyToManyField(
+            A,
+            related_name=related_name,
+        )
+
+        @final
+        class Meta:
+            verbose_name_plural = title
+
+    class FieldSchema(BaseSchema[A]):
+        config: SchemaConfig[A] = SchemaConfig(
+            model=A, fields={"id": Infer, related_name: Infer}
+        )
+
+    openapi_schema = FieldSchema.model_json_schema()
+    assert openapi_schema["properties"][related_name]["title"].strip() == title.strip()
+
+
+def test_ForeignKey_reverse_relationship_title_is_set_from_verbose_name() -> None:
+    """Test that the reverse relation field title is set from the verbose name."""
+    title = "This is a test title for the reverse relationship field."
+    related_name = "b_set"
+
+    @final
+    class A(models.Model):
+        pass
+
+    @final
+    class B(models.Model):  # pyright: ignore[reportUnusedClass]
+        a = models.ForeignKey(
+            A,
+            related_name=related_name,
+            on_delete=models.CASCADE,
+        )
+
+        @final
+        class Meta:
+            verbose_name_plural = title
+
+    class FieldSchema(BaseSchema[A]):
+        config: SchemaConfig[A] = SchemaConfig(
+            model=A, fields={"id": Infer, related_name: Infer}
+        )
+
+    openapi_schema = FieldSchema.model_json_schema()
+    assert openapi_schema["properties"][related_name]["title"].strip() == title.strip()
