fe1edd1c149e80f0f40ca6d2ef29879a4cde83f5
Phuong Nguyen <phuong.fi.91@gmail.com>
2025-06-05 16:18:17 +0300

    fix: JSONField validation should accept python object

--- django2pydantic/handlers/json.py
+++ django2pydantic/handlers/json.py
@@ -1,15 +1,28 @@
 """Handler for JSON fields."""
 
-from typing import override
+import json
+from types import UnionType
+from typing import Annotated, Any, override
 
 from django.db import models
-from pydantic import Json
+from pydantic import BeforeValidator, Json
 
 from django2pydantic.handlers.base import DjangoFieldHandler
-from django2pydantic.types import GetType, SetType
+from django2pydantic.types import GetType, SetType, SupportedPydanticTypes
 
 
-class JSONFieldHandler(DjangoFieldHandler[models.JSONField[SetType, GetType]]):
+def ensure_json_str(value: Any) -> str:  # pyright: ignore [reportExplicitAny]
+    """Ensure the value being validated is a string.
+
+    Pydantic's Json type expects a string representation of JSON, passing in a python
+    object will raise validation error. This function ensures the value is a string.
+    """
+    if isinstance(value, str):
+        return value
+    return json.dumps(value)
+
+
+class JSONFieldHandler(DjangoFieldHandler[models.JSONField[SetType, GetType]]):  # pyright: ignore [reportExplicitAny]
     """Handler for JSON fields."""
 
     @classmethod
@@ -20,5 +33,13 @@ class JSONFieldHandler(DjangoFieldHandler[models.JSONField[SetType, GetType]]):
     @override
     def get_pydantic_type_raw(
         self,
-    ):
-        return Json
+    ) -> UnionType | SupportedPydanticTypes | list[SupportedPydanticTypes]:
+        return Json  # type: ignore[no-any-return]
+
+    @override
+    def get_pydantic_type(
+        self,
+    ) -> UnionType | SupportedPydanticTypes | list[SupportedPydanticTypes]:
+        return Annotated[  # type: ignore[return-value]
+            super().get_pydantic_type(), BeforeValidator(ensure_json_str)
+        ]
